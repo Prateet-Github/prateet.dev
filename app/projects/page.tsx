00e609f6322091e20f2ea928244f56ce2cab9cd8
Prateet Tiwari <prateettiwari29@gmail.com>
2026-01-18 19:38:10 +0530

    Remove Projects page component and clean up unused styles in globals.css and add favicon

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-const Projects = () => {
-  return (
-    <main className="mx-auto max-w-6xl min-h-screen flex items-center justify-center border border-border">
-      <h2>All Projects will be listed here</h2>
-    </main>
-  );
-};
-
-export default Projects;
